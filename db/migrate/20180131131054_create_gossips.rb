91404f88411801c39c1507f1d1f633fafa14efa8
Rafael <rafael@rafael.lan>
2018-01-31 15:30:01 +0100

    First view

--- db/migrate/20180131131054_create_gossips.rb
+++ db/migrate/20180131131054_create_gossips.rb
@@ -1,8 +1,13 @@
+#La classe CreateGossips hérite de ActiveRecord
 class CreateGossips < ActiveRecord::Migration[5.1]
   def change
+    #La table va avoir plusieurs colonnes
     create_table :gossips do |t|
+      #Avant la première colonne il en une qui contient les id. Donc première ligne à l'id 1...
+      #On cré une nouvelle colonne qui prendra une string et qui s'appel "anonimous_autor"
       t.string :anonimous_autor
       t.string :content
+      #On cré 2 colones, "created at" et "updated at"
       t.timestamps
     end
   end
