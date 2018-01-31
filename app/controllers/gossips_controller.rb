440aa603fb6c1aaef4c13b38a4d375a1d4982a69
Rafael <rafael@rafael.lan>
2018-01-31 18:15:34 +0100

    Gossip End

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,6 +1,50 @@
 class GossipsController < ApplicationController
 
-  def index()
+  def create()
+    #instance de la class Gossips
+    @gossip = Gossip.new(gossip_params)
+    @gossip.save
+    redirect_to @gossip
+  end
+
+  def destroy
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+
+    redirect_to gossips_path
+  end
+
+  def edit
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def new()
+    #instance de la class Gossips
+    @gossip = Gossip.new
 
   end
+
+  def index()
+    @gossips = Gossip.all
+  end
+
+  def show()
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def update
+    @gossip = Gossip.find(params[:id])
+
+    if @gossip.update(gossip_params)
+      redirect_to @gossip
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def gossip_params
+    params.require(:gossip).permit(:anonimous_autor, :content)
+  end
+
 end
